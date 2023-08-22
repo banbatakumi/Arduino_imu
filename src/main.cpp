25fef2fcee5e09cf4bbe0c702de08cc56975bed2
banbatakumi <bantech0210@gmail.com>
2023-08-22 23:54:01 +0900

    Lチカ

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,14 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+      pinMode(A2, OUTPUT);
+      pinMode(A3, OUTPUT);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+      digitalWrite(A2, LOW);
+      digitalWrite(A3, HIGH);
+      delay(100);
+      digitalWrite(A2, HIGH);
+      digitalWrite(A3, LOW);
+      delay(100);
 }
